a1691806144eb53e8c50a7e8f03ee8e28b4c556f
GitHub Action <action@github.com>
2023-09-14 05:06:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/sakumatakuya/findusagerecursiveforintelij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sakumatakuya/findusagerecursiveforintelij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sakumatakuya.findusagerecursiveforintelij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/sakumatakuya/findusagerecursiveforintelij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sakumatakuya/findusagerecursiveforintelij/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sakumatakuya.findusagerecursiveforintelij.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sakumatakuya.findusagerecursiveforintelij.MyBundle
+import com.github.sakumatakuya.findusagerecursiveforintelij.services.MyProjectService
 import javax.swing.JButton
 
 
